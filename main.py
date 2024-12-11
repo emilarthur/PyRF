7876011a6c2ea92e7c0ecc5e3eb73989dd7bd1d3
emilarthur <emilseilov@gmail.com>
2024-12-11 16:31:12 +0100

    Main from jupyter to py

---
+++ main.py
@@ -0,0 +1,7 @@
+from RF.microstrip import *
+from RF.microstrip.mlcalc import *
+
+freq = 3e9
+
+sub = Substrate(name="sub", h=0.635e-3, t=35e-6, er=10.2, tanD=0)
+W, L = synthesize(Z0=50,f=freq, theta=90, substrate=sub,thicknesss=True, disp=True)
