59ae0e806f6d7e0364b72235b31114687732268d
emilarthur <75788796+emilarthur@users.noreply.github.com>
2024-12-16 01:33:14 +0100

    deleted an extra 's' in thickness

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from microstrip.mlcalc import *
 freq = 3e9
 
 sub = Substrate(name="sub", h=0.635e-3, t=35e-6, er=10.2, tanD=0)
-W, L = synthesize(Z0=50,f=freq, theta=90, substrate=sub,thicknesss=True, disp=True)
+W, L = synthesize(Z0=50,f=freq, theta=90, substrate=sub,thickness=True, disp=True)
